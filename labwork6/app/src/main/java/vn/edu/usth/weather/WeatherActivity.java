43b86f53265d999f247224eef9a07775da2080a7
Nguyen Viet Nhan <74595111+nhanviet1@users.noreply.github.com>
2021-10-13 00:53:48 +0700

    lab6

---
+++ labwork6/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,64 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+    public static final String TAG = "WeatherActivity";
+
+    /*@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        ForecastFragment lmeow = ForecastFragment.newInstance("", "");
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, lmeow).commit();
+        Log.i("Weather", "onCreate: ");
+    }*/
+
+    private void commit() {
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i("Weather", "onStart: ");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("Weather", "onResume: ");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i("Weather", "onPause: ");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i("Weather", "onStop: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("Weather", "onDestroy: ");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        ForecastFragment mup = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.mup, mup).commit();
+        ForecastFragment mdown = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(R.id.mdown, mup).commit();
+    }
+}
