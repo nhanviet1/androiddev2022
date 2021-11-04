119985e47421a01e326973f22ebafc331be80bed
Nguyen Viet Nhan <74595111+nhanviet1@users.noreply.github.com>
2021-11-05 00:05:44 +0700

    lab14

---
+++ lab14/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,160 @@
+package vn.edu.usth.weather;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        PagerAdapter lmeow = new HomeFragmentPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(lmeow);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(pager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater1 = getMenuInflater();
+        inflater1.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.refresh:
+                MyAsyncTasks myAsyncTasks = new MyAsyncTasks();
+                myAsyncTasks.execute();
+                return true;
+
+            case R.id.settings:
+                startActivity(new Intent(WeatherActivity.this, PreferenceActivity.class));;
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("weather", "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("weather", "onResume: ");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("weather", "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("weather", "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("weather", "onDestroy: ");
+    }
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private String titles[] = new String[] { "F1", "F2", "F3" };
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return PAGE_COUNT; // number of pages for a ViewPager
+        }
+        @Override
+        public Fragment getItem(int page) {
+            switch (page) {
+                case 0: return new WeatherAndForecastFragment();
+                case 1: return new WeatherAndForecastFragment();
+                case 2: return new WeatherAndForecastFragment();
+            }
+            return new Fragment();
+        }
+        @Override
+        public CharSequence getPageTitle(int page) {
+            return titles[page];
+        }
+    }
+
+    final Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            String content = msg.getData().getString("server_response");
+            Toast.makeText(WeatherActivity.this, content, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public class MyAsyncTasks extends AsyncTask<String, String, String>
+    {
+        @Override
+        protected void onPreExecute()
+        {
+
+        }
+        @Override
+        protected String doInBackground(String... params)
+        {
+            try
+            {
+                Thread.sleep(5000);
+            } catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onProgressUpdate(String... values)
+        {
+
+        }
+        @Override
+        protected void onPostExecute(String bitmap)
+        {
+            Toast.makeText(WeatherActivity.this, "ψ(｀∇´)ψ", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
